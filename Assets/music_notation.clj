97766334e0a5d1a9f2f4edaadbd61fb80ea434bf
Paul M. Christian <paulmchristian@gmail.com>
2018-03-03 13:45:56 -0600

    code cleanup

--- Assets/music_notation.clj
+++ Assets/music_notation.clj
@@ -6,6 +6,15 @@
   (:import (UnityEngine Color GameObject Mesh MeshFilter MeshRenderer Mathf TextMesh Vector2 Vector3))
   )
 
+(def lexicon "examples and definitions of terms used"
+  {
+   :note-value "1/2 - the rhythmic length of a note event"
+   :note "60 - the MIDI number corresponding to pitch"
+   :pitch ":c4 - a keyword corresponding to pitch"
+   :pitch-class ":c - a keyword corresponding to pitch class with no octave"
+   :glyph ":note-2-up - a keyword specifying the font character to render"
+   })
+
 ; Trying to make the glyph from scratch but seems font won't load dynamically
 ;(let [obj (GameObject. "abc")
 ;      tm (cmpt+ obj UnityEngine.TextMesh)]
@@ -15,10 +24,10 @@
 ;  (set! (. tm text) (str \uE050))
 ;  )
 
-(def dx 1)
-(def dy 0.25)
+(def dx "smallest unit in x-dimension for rendering glyphs" 1)
+(def dy "smallest unit in y-dimension for rendering glyphs" 0.25)
 
-(defn create-glyph
+(defn +glyph
   "creates a glyph at (x,y,0). returns the GameObject"
   [glyph x y]
   (let [go (instantiate (object-named "GlyphTemplate"))
@@ -32,14 +41,16 @@
     go
     ))
 
-(defn create-staff [n y]
+(defn +staff [n y]
   (doall (->> (range (* 3 n))
-              (map #(create-glyph :staff-5 % y)))))
+              (map #(+glyph :staff-5 % y)))))
 
-(defn create-evt [t y note-value accidental dotted]
+(defn +note
+  "creates a note on the staff with optionally accidental and dot"
+  [t y note-value accidental dotted]
   (let [x0 (* 3 t)]
-    (when accidental (create-glyph accidental x0 y))
-    (create-glyph
+    (when accidental (+glyph accidental x0 y))
+    (+glyph
       (case note-value
         1 :note-1
         1/2 :note-2-up
@@ -51,20 +62,20 @@
       (+ 1 x0) ; make room for accidental
       y
       )
-    (when dotted (create-glyph :note-dot (+ 2 x0) y))))
+    (when dotted (+glyph :note-dot (+ 2 x0) y))))
 
 
-(defn note->pitch-class-flats
+(defn note->pitch-class-b-bias
   "returns pitch class given note, with flats as accidentals"
   [note]
   (nth (cycle [:c :db :d :eb :e :f :gb :g :ab :a :bb :b]) note))
 
-(defn note->pitch-class-sharps
+(defn note->pitch-class-#-bias
   "returns pitch class given note, with sharps as accidentals"
   [note]
   (nth (cycle [:c :c# :d :d# :e :f :f# :g :g# :a :a# :b]) note))
 
-(defn octave
+(defn octave-of-note
   "returns the octave # of a given note see: https://en.wikipedia.org/wiki/Scientific_pitch_notation#Table_of_note_frequencies"
   [note]
   (- (int (/ note 12)) 1))
@@ -77,112 +88,125 @@
        (drop-while #(< % 0))
        (take-while #(< % 128))))
 
-(def notes-in-c (set (notes-in-key 0)))
+(def notes-in-c
+  "a set of all notes in key of c"
+  (set (notes-in-key 0)))
 
 (defn strip-accidental
-  "returns the natural pitch class"
+  "returns the natural pitch class of provided note or pitch-class"
   [pitch-class]
-  (keyword (str (nth (str pitch-class) 1))))
+  (keyword (subs (name pitch-class) 0 1)))
 
 (defn extract-accidental
   "returns the accidental or nil"
   [pitch-class]
-  (if (< (count (str pitch-class)) 3)
+  (if (< (count (name pitch-class)) 2)
     nil
-    (keyword (apply str (drop 2 (str pitch-class))))))
+    (keyword (subs (name pitch-class) 1 2))))
 
-(defn note-data [natural-pitch-class accidental octave]
-  {
-   :pc natural-pitch-class
-   :acc accidental
-   :oct octave
-   })
+
+
+
 
 (def keys-data ; TODO cb and c# ??
-  (->> [:sharp :flat] ; to get the accidentals in each key, we walk in either direction on the circle of 5ths
-       (map (fn [bias]
-              (let [
-                    note->pitch-class (case bias ;either prefer sharp or flat when spelling notes
-                                        :flat note->pitch-class-flats
-                                        :sharp note->pitch-class-sharps)
-                    interval (case bias ; which way do we walk the circle of 5ths?
-                               :flat 5 ; walking by 4ths, flat
-                               :sharp 7)
-                    circle-order (->> (range 12)
-                                      (map #(mod (* interval %) 12))
-                                      (map note->pitch-class))
-                    ] ;walking by 5ths, sharp
-                (->> (range 7)
-                     (map #(mod (* interval %) 12))
-                     (map (fn [note]
-                            (let [key (note->pitch-class note)
-                                  accidentals (->> (notes-in-key note) ;all notes in this note's key
-                                                   (take 7) ; there are only 7 pitch classes in a key
-                                                   (map #(when-not ; a note not in the key of c is an accidental
-                                                           (contains? notes-in-c %)
-                                                           (note->pitch-class %))) ; get its pitch-class
-                                                   (filter identity) ; filter nil (non-accidentals)
-                                                   (set) ; it's not in the order we want. more useful as a set
-                                                   )
-                                  accidentals-renamed (case key ; when the key...
-                                                        :f# (union #{:e#} accidentals) ; add :e#
-                                                        :gb (union #{:cb} accidentals) ; add :cb
-                                                        accidentals ; otherwise leave it alone
-                                                        )
-                                  circle-order-renamed (->> circle-order
-                                                            (map #(cond
-                                                                    (and (= key :f#)
-                                                                         (= % :f))
-                                                                    :e#
+  ; to get the accidentals in each key, we walk in either direction on the circle of 5ths
+  (->> [:sharp :flat]
+       (map
+         (fn [bias]
+           (let [
+                 [note->pitch-class interval]
+                 (case bias ;either prefer sharp or flat when spelling notes
+                   :flat [note->pitch-class-b-bias 5] ; prefer flats, walk by 4ths
+                   :sharp [note->pitch-class-#-bias 7]) ; prefer sharps, walk by 5ths
 
-                                                                    (and (= key :gb)
-                                                                         (= % :b))
-                                                                    :cb
+                 circle-order ; order of notes around the circle of 5ths
+                 (->> (range 12)
+                      (map #(mod (* interval %) 12))
+                      (map note->pitch-class))
+                 ]
+             (->> (range 7) ; 7 keys in each direction (c is duplicated then deduped later)
+                  (map #(mod (* interval %) 12))
+                  (map (fn [note]
+                         (let [key (note->pitch-class note)
 
-                                                                    true % ; otherwise leave it alone
-                                                                    )))
-                                  accidentals-ordered (->> circle-order-renamed
-                                                           (filter #(contains? accidentals-renamed %)))
-                                  note-spellings
-                                  (->> (range 128)
-                                       (map #(let [pc (note->pitch-class %)
-                                                   spelling (cond
-                                                              (and (= key :f#) (= pc :e))
-                                                              :en
+                               accidentals
+                               (->> (notes-in-key note) ;all notes in this note's key
+                                    (take 7) ; there are only 7 pitch classes in a key
+                                    (map #(when-not ; a note not in the key of c is an accidental
+                                            (contains? notes-in-c %)
+                                            (note->pitch-class %))) ; get its pitch-class
+                                    (filter identity) ; filter nil (non-accidentals)
+                                    (set) ; it's not in the order we want. more useful as a set
+                                    )
 
-                                                              (and (= key :f#) (= pc :f))
-                                                              :e
+                               accidentals-renamed
+                               (case key ; when the key...
+                                 :f# (union #{:e#} accidentals) ; add :e#
+                                 :gb (union #{:cb} accidentals) ; add :cb
+                                 accidentals ; otherwise leave it alone
+                                 )
 
-                                                              (and (= key :gb) (= pc :c))
-                                                              :cn
+                               circle-order-renamed
+                               (->> circle-order
+                                    (map #(cond
+                                            (and (= key :f#) (= % :f)) :e#
+                                            (and (= key :gb) (= % :b)) :cb
+                                            true %)))
 
-                                                              (and (= key :gb) (= pc :b))
-                                                              :c
+                               accidentals-ordered
+                               (->> circle-order-renamed
+                                    (filter #(contains? accidentals-renamed %)))
 
-                                                              (contains? accidentals pc) ;accidentals
-                                                              (strip-accidental pc) ; are spelled without accidental
+                               note-spellings
+                               (->> (range 128)
+                                    (map #(let [
+                                                pc
+                                                (note->pitch-class %)
 
-                                                              (contains? ; base pc of accidentals
-                                                                (set (map strip-accidental accidentals))
-                                                                pc)
-                                                              (keyword (str (last (str (strip-accidental pc))) "n")) ; are spelled "natural"
+                                                spelling
+                                                (cond
+                                                  ; corrections for :f# and :gb
+                                                  (and (= key :f#) (= pc :e))
+                                                  :en
 
-                                                              true ;otherwise
-                                                              pc ; leave it alone
-                                                              )
-                                                   octave (octave %)]
-                                               {
-                                                :pc  (strip-accidental spelling)
-                                                :acc (extract-accidental spelling)
-                                                :oct octave
-                                                })))
-                                  ]
-                              {
-                               :key         key
-                               :note        note
-                               :accidentals accidentals-ordered
-                               :spellings   (apply vector note-spellings) ; faster lookup as vector
-                               })))))))
+                                                  (and (= key :f#) (= pc :f))
+                                                  :e
+
+                                                  (and (= key :gb) (= pc :c))
+                                                  :cn
+
+                                                  (and (= key :gb) (= pc :b))
+                                                  :c
+
+                                                  ; accidentals are spelled without accidental
+                                                  (contains? accidentals pc)
+                                                  (strip-accidental pc)
+
+                                                  ; base pc of accidentals are spelled "natural"
+                                                  (contains?
+                                                    (set (map strip-accidental accidentals))
+                                                    pc)
+                                                  (keyword (str (name (strip-accidental pc))
+                                                                "n"))
+
+                                                  ;otherwise leave it alone
+                                                  true pc)
+
+                                                octave
+                                                (octave-of-note %)
+                                                ]
+                                            {
+                                             :pc  (strip-accidental spelling)
+                                             :acc (extract-accidental spelling)
+                                             :oct octave
+                                             })))
+                               ]
+                           {
+                            :key         key
+                            :note        note
+                            :accidentals accidentals-ordered
+                            :spellings   (apply vector note-spellings) ; faster lookup as vector
+                            })))))))
        (flatten) ; combine both sequences. the key of c will be present in both.
        (#(zipmap (map :key %) %)) ; turn it into a map so we can dedupe and look up data by key
        )
@@ -194,19 +218,17 @@
   [key note]
   (nth (:spellings (key keys-data)) note))
 
-(defn note-spelling->y-and-accidental ":c#4 => {:y 0 :acc :sharp}" [spelling])
-
 (defn create-grand-staff []
-  (create-glyph :staff-5 -2 2)
-  (create-glyph :staff-5 -1 2)
-  (create-glyph :clef-g -2 4)
+  (+glyph :staff-5 -2 2)
+  (+glyph :staff-5 -1 2)
+  (+glyph :clef-g -2 4)
 
-  (create-glyph :staff-5 -2 -10)
-  (create-glyph :staff-5 -1 -10)
-  (create-glyph :clef-f -2 -4)
+  (+glyph :staff-5 -2 -10)
+  (+glyph :staff-5 -1 -10)
+  (+glyph :clef-f -2 -4)
 
-  (create-staff 10 2)
-  (create-staff 10 -10))
+  (+staff 10 2)
+  (+staff 10 -10))
 
 (create-grand-staff)
 
@@ -224,7 +246,9 @@
        ))
 
 
-(defn create-keysig [key min-staff-y max-staff-y]
+(defn +keysig
+  "creates the glyphs of the given key signature within a vertical range on the staff"
+  [key min-staff-y max-staff-y]
   (doall
     (->> (:accidentals (key keys-data))
          (map-indexed #(let [pitch-class (nth (str %2) 1)
@@ -237,39 +261,39 @@
                              accidental (case (last (str %2))
                                           \# :sharp
                                           \b :flat)]
-                         (create-glyph accidental x y))))))
+                         (+glyph accidental x y))))))
 
-(defn create-keysig-treble [key]
-  (create-keysig key 4 12))
-(defn create-keysig-bass [key]
-  (create-keysig key -10 -3))
+(defn +keysig-treble [key]
+  (+keysig key 4 12))
+(defn +keysig-bass [key]
+  (+keysig key -10 -3))
 
-(create-keysig-treble :gb)
-(create-keysig-bass :gb)
+(+keysig-treble :gb)
+(+keysig-bass :gb)
 
 
 (comment
 
-  (spell-note :c 60)
+  (spell-note :f# 60)
 
   (:accidentals (:cb keys-data))
 
   (do
-    (create-glyph :ledger 2 0)
-    (create-glyph :ledger 2 12)
+    (+glyph :ledger 2 0)
+    (+glyph :ledger 2 12)
 
-    (create-glyph :flat 0 0)
-    (create-glyph :note-1 1 0)
+    (+glyph :flat 0 0)
+    (+glyph :note-1 1 0)
 
-    (create-evt 3 2 1/2 :flat true)
-
-    (->> (range 10)
-         (map #(create-evt % (- % 5) 1 :flat false)))
+    (+note 3 2 1/2 :flat true)
 
     (->> (range 10)
-         (map #(create-evt % (+ % 5) 1 :sharp false)))
+         (map #(+note % (- % 5) 1 :flat false)))
 
-    (create-glyph :flat 2 1)
-    (create-glyph :note-1 3 1)
+    (->> (range 10)
+         (map #(+note % (+ % 5) 1 :sharp false)))
+
+    (+glyph :flat 2 1)
+    (+glyph :note-1 3 1)
     ))
 
