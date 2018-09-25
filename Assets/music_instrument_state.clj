9a9417861a53cd1fb113bf68c099a7d72606f829
Paul M. Christian <paulmchristian@gmail.com>
2018-09-24 22:36:50 -0500

    receive /keystation/notes in music-instrument-state

--- Assets/music_instrument_state.clj
+++ Assets/music_instrument_state.clj
@@ -4,27 +4,41 @@
 
 ; Make sure "Filter Duplicates" is unchecked in OscIn component
 
-(def s (atom {
-              :notes (->> (range 128) ;TODO: use sparse data instead of vel 0?
+(def s (let [notes-initial
+             {
+              :notes (->> (range 128)                       ;TODO: use sparse data instead of vel 0?
                           (map #(vector % 0))
                           (flatten)
                           (apply hash-map))
-              }))
+              }]
+         (atom {
+                :default notes-initial
+                :keystation notes-initial
+                })))
 
-(defn- on-note [osc-msg]
+(defn- on-note [instrument osc-msg]
   (let [[note vel] (vec (. osc-msg (get_args)))]
-    (swap! s assoc-in [:notes note] vel)
+    (swap! s assoc-in [instrument :notes note] vel)
     (log (str note " " vel))))
 
-(listen "/note" #'on-note)
+(listen "/keystation/note" (fn [osc-msg] (on-note :keystation osc-msg)))
 
 (defn get-notes
   "returns map of currently played notes and their velocities"
-  []
-  (->> (:notes @s)
-       (filter #(> (second %) 0))
-       (flatten)
-       (apply hash-map)))
+  ([instrument]
+   (->> (get-in @s [instrument :notes])
+        (filter #(> (second %) 0))
+        (flatten)
+        (apply hash-map)))
+  ([] (get-notes :default)))
+
+(get-notes :keystation)
+
 
 (defn get-note "returns current velocity of note"
-  [n] (get (:notes @s) n))
+  ([instrument n] (get-in @s [instrument :notes n]))
+  ([n] (get-note :default)))
+
+(get-note :keystation 50)
+
+(@s :keystation)
